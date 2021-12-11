085974000848de7dd83ac834bc6acf18c0c39f84
Emircan Ağaç <thesadson@gmail.com>
2021-12-11 11:07:34 +0300

    Infinite.py

--- infinite.py
+++ infinite.py
@@ -1,6 +1,5 @@
 #Understanding ramanujan summation
 print("The Ramanujan Summation: 1 + 2 + 3 + ⋯ + ∞ = -1/12?")
-input("press enter to start:")
 
 def translate(A):
     b =len(A) 
@@ -11,16 +10,14 @@ def translate(A):
             print(A[i],end = '+')
 
 S = [1,2,3,4,5,6,7,8] # S=infinite array 1+2+3+4+5+...
-
 S1  =  [1,-1, 1,-1, 1,-1, 1,-1,]
 s1= 1/2
 translate(S)
 print("= S1 Array")
 print("S1 sum = 1 or 0 = 1+0/2 -> ", s1 ,"≌ 1/2")
-input("its ok? --> Press enter:")
 
 S2  =  [1,-2, 3,-4, 5,-6, 7,-8]
-print("S2: ",S2)
+print("\nS2: ",S2)
 S20 =  [0, 1,-2, 3,-4, 5,-6,-7,8,-9]
 S2plusS20=[]
 for i in range(len(S2)-1):
@@ -30,24 +27,21 @@ translate(S2plusS20)
 print("= S2 + S2 = 2*S2",)
 s2=1/4
 print("2*(S2)= 1/2 ===> S2 = ",s2 ,"≌ 1/4")
-input("its ok? --> Press enter:")
 
 SminesS2 =[]
 #S - S2 =?
 for j in range(len(S)-1):
     rslt = S[j]-S2[j]
     SminesS2.append(rslt)
-print("S: ", end = "")
+print("\nS: ", end = "")
 translate(S)
 print("\nS2:" , end ="")
 translate(S2)
 print("\nS - S2 =",SminesS2)
-input("its ok? --> Press enter")
-
 print("S - S2 = 4(1+2+3+4+5+...)")
 print("S - S2 = 4*(S) \n-S2 = 3*(S) ===> -1/4 = 3*(S)")
 P = translate(S)
-print(-s2/3, "= S ≌ -1/12")
 s = -1/12
+print(-s2/3, "= S ≌ -1/12\n")
 
 print("✔️   The Ramanujan Summation: 1 + 2 + 3 + ⋯ + ∞ = -1/12 ✔️")
